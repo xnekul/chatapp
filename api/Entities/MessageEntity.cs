d5cd02387b508c93228795265f7922fd4cb0ed27
STEPAN <stepan.nekula@gmail.com>
2024-06-25 16:45:15 +0200

    Fix Message Entity schema

--- api/Entities/MessageEntity.cs
+++ api/Entities/MessageEntity.cs
@@ -8,7 +8,9 @@ namespace api.Entities
     public class MessageEntity : IEntity
     {
         public int Id { get; set; }
-        public int ChatId { get; set; }
+        public string Content { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+        public int RoomId { get; set; }
         public RoomEntity Room { get; set; } = new RoomEntity();
         public int AuthorId { get; set; }
         public UserEntity Author { get; set; } = new UserEntity();
---
+++ api/Migrations/20240624102934_MessageEntity_Changes.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class MessageEntity_Changes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ChatId",
+                table: "Messages");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Content",
+                table: "Messages",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "Messages",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Content",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Messages");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ChatId",
+                table: "Messages",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
